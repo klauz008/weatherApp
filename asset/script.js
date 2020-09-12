08b8be64733ca3e17e2fc692ff957a26bbfcfa63
Nicholas Bassey <nickyawat@gmail.com>
2020-09-12 10:38:39 +0100

    syntax adjustment

--- asset/script.js
+++ asset/script.js
@@ -3,19 +3,19 @@
  let searchMethod;  
 
  function getSearchMethod(searchTerm) {
-     if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
-         searchMethod = 'zip';
+    if(searchTerm.length === 5 && Number.parseInt(searchTerm) + "" === searchTerm)
+        searchMethod = "zip";
     else 
-         searchMethod = 'q';
+        searchMethod = "q";
  }
 
 function searchWeather(searchTerm) {
     getSearchMethod(searchTerm);
     fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
-         .then((result) => {
-             return result.json();
-         }).then((res) => {
-             init(res);
+        .then((result) => {
+        return result.json();
+        }).then((res) => {
+        init(res);
     });
 }
 
@@ -47,37 +47,37 @@ function init(resultFromServer) {
         break;
     }
 
-    let weatherDescriptionHeader = document.getElementById('weatherDescriptionHeader');
-    let temperatureElement = document.getElementById('temperature');
-    let humidityElement = document.getElementById('humidity');
-    let windSpeedElement = document.getElementById('windSpeed');
-    let cityHeader = document.getElementById('cityHeader');
+    let weatherDescriptionHeader = document.getElementById("weatherDescriptionHeader");
+    let temperatureElement = document.getElementById("temperature");
+    let humidityElement = document.getElementById("humidity");
+    let windSpeedElement = document.getElementById("windSpeed");
+    let cityHeader = document.getElementById("cityHeader");
 
-    let weatherIcon = document.getElementById('documentIconImg');
+    let weatherIcon = document.getElementById("documentIconImg");
     weatherIcon.src = `http://openweathermap.org/img/w/${resultFromServer.weather[0].icon}.png`;
 
     let resultDescription = resultFromServer.weather[0].description;
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
-    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176;';
-    windSpeedElement.innerHTML = 'Winds at  ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + "C" + "&#176;";
+    windSpeedElement.innerHTML = "Winds at  " + Math.floor(resultFromServer.wind.speed) + " m/s" ;
     cityHeader.innerHTML = resultFromServer.name;
-    humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity +  '%';
+    humidityElement.innerHTML = "Humidity levels at " + resultFromServer.main.humidity +  "%";
 
     setPositionForWeatherInfo();
 }
 
 function setPositionForWeatherInfo() {
-    let weatherContainer = document.getElementById('weatherContainer');
+    let weatherContainer = document.getElementById("weatherContainer");
     let weatherContainerHeight = weatherContainer.clientHeight;
     let weatherContainerWidth = weatherContainer.clientWidth;
 
     weatherContainer.style.left = `calc(50% - ${weatherContainerWidth/2}px)`;
     weatherContainer.style.top = `calc(50% - ${weatherContainerHeight/1.3}px)`;
-    weatherContainer.style.visibility = 'visible';
+    weatherContainer.style.visibility = "visible";
 }
 
- document.getElementById('searchBtn').addEventListener('click', ()=> {
-     let searchTerm = document.getElementById('searchInput').value;
+ document.getElementById("searchBtn").addEventListener("click", ()=> {
+     let searchTerm = document.getElementById("searchInput").value;
      if(searchTerm)
-         searchWeather(searchTerm);
+        searchWeather(searchTerm);
  });
